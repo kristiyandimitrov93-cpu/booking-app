2bc1926fa516aa3bb910ad46b8bc42d723402ef6
kristiyandimitrov93-cpu <kristiyan.dimitrov93@gmail.com>
2026-01-28 21:49:04 +0200

    removing template styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import { Container, Navbar } from 'react-bootstrap'
 import './App.css'
 import { BookingForm } from './components/BookingForm'
+import { BookingsView } from './components/BookingsView'
+import { Calendar } from 'lucide-react'
 
 function App() {
 
-  return (
-    <>
+  return (<>
+    <Navbar bg="primary" variant="dark" className="mb-4">
+      <Container fluid>
+        <Navbar.Brand className="d-flex align-items-center gap-2">
+          <Calendar size={24} />
+          Bookings Manager
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
 
-      <BookingForm />
-      {/* /<BookingsView /> */}
-    </>
+    <Container fluid>
+      <div className="row g-4 align-items-start">
+        <div className="col-12 col-lg-4 col-xl-3">
+          <BookingForm />
+        </div>
+
+        <div className="col-12 col-lg-8 col-xl-9">
+          <BookingsView />
+        </div>
+      </div>
+    </Container>
+  </>
   )
 }
 
